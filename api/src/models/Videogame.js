a89556f8d99f8f13da8f7a9b78e1c66d38a7090f
SebastianGaviriaM <70300843+SebastianGaviriaM@users.noreply.github.com>
2023-02-22 15:43:13 -0500

    BD1
    Primer diseño de la base de datos

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type:DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    platform:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    releaseDate:{
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    rating:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    image:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
 };
